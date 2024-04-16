17b6f8411ffc396d216c9a3229deb566fdf87f0d
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2024-04-16 14:45:21 -0400

    avances boton dentro de carousel
    Signed-off-by: figonzal1 <felipe.gonzalezalarcon94@gmail.com>

--- astro/centroamai.cl/src/components/MyCarousel.tsx
+++ astro/centroamai.cl/src/components/MyCarousel.tsx
@@ -2,12 +2,14 @@ import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel";
 
 import Autoplay from "embla-carousel-autoplay";
 import p1 from "@assets/images/carousel/fotos_portada1.jpeg";
+import { Button } from "@/components/ui/button";
 
 const MyCarousel = () => {
   return (
     <Carousel
       opts={{
         loop: true,
+        active: false,
         duration: 60,
       }}
       plugins={[
@@ -23,9 +25,14 @@ const MyCarousel = () => {
           <img
             src={p1.src}
             alt="Home"
-            className="object-cover w-full h-[80vh] opacity-90 brightness-50 blur-sm"
+            className="absolute inset-0 object-cover w-full h-full opacity-90 brightness-50 blur-sm"
           />
-          <div className="absolute inset-0 bg-gradient-to-br from-[#7FAEB5] to-transparent"></div>
+          <div className="absolute inset-0 bg-gradient-to-br from-[#7FAEB5] to-transparent flex justify-center items-center">
+            <Button className="absolute bg-white text-[#3FAEBD] font-bold py-2 px-4 rounded-3xl uppercase">
+              Reserva aquí
+            </Button>
+            <div className="mt-2 ms-2 bg-gray-700 rounded-3xl w-32 h-10"></div>
+          </div>
         </CarouselItem>
         <CarouselItem>
           <img
