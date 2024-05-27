fcdba26861c7e623d25c7d50905dda17fb473d72
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2024-05-26 20:58:46 -0400

    avances en navbar
    Signed-off-by: figonzal1 <felipe.gonzalezalarcon94@gmail.com>

--- astro/centroamai.cl/src/components/Navastro.tsx
+++ astro/centroamai.cl/src/components/Navastro.tsx
@@ -26,41 +26,67 @@ const Navastro = () => (
               Pediatría
             </NavigationMenuTrigger>
             <NavigationMenuContent>
-              <ul className="flex gap-8 p-6 md:w-[400px] lg:w-[600px]">
-                <li className="basis-1/2">
+              <div className="flex gap-8 p-6 md:w-[400px] lg:w-[600px]">
+                
+                <div className="basis-1/3">
                   <NavigationMenuLink asChild>
                     <a
-                      className="flex h-full w-full select-none flex-col justify-end rounded-3xl bg-gradient-to-b from-muted/50 to-muted no-underline outline-none focus:shadow-md"
+                      className="flex h-full w-full select-none flex-col justify-end rounded-3xl bg-gradient-to-b from-muted/50 to-muted no-underline outline-none focus:shadow-md relative"
                       href="/"
                     >
                       <img
                         src="nav/pediatria.jpg"
                         alt=""
-                        className="object-contain rounded-t-3xl"
+                        className="object-cover h-72 opacity-50 rounded-3xl "
                       />
 
-                      <div className="p-6">
-                        <div className="mb-2 mt-4 text-lg font-medium">
-                          shadcn/ui
-                        </div>
-                        <p className="text-sm leading-tight text-muted-foreground">
-                          Beautifully designed components that you can copy and
-                          paste into your apps. Accessible. Customizable. Open
-                          Source.
-                        </p>
+                      <div className="absolute mb-2 mt-4 text-lg font-medium">
+                        shadcn/ui
                       </div>
                     </a>
                   </NavigationMenuLink>
-                </li>
-                <div className="basis-1/2">
-                  <h2 className="font-bold">Pediatría 1</h2>
-                  <h3>Subtitulo pediatría</h3>
                 </div>
-              </ul>
+
+
+                <div className="basis-2/3 bg-white">
+                  <div className="flex gap-5">
+                    <ul className="space-y-5">
+                      <li className="space-y-2 hover:bg-cgreen/10 rounded-xl p-2 transition-colors duration-300 ease-in-out">
+                        <h2 className="font-bold leading-none">Servicio 1</h2>
+                        <p className="line-clamp-2 text-sm leading-snug text-cgray">
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Ad eaque saepe voluptate odit soluta eveniet
+                          quam itaque officiis explicabo perferendis
+                        </p>
+                      </li>
+
+                      <li className="space-y-2 hover:bg-cgreen/10 rounded-xl p-2 transition-colors duration-300 ease-in-out">
+                        <h2 className="font-bold leading-none">Servicio 2</h2>
+                        <p className="line-clamp-2 text-sm leading-snug text-cgray">
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Ad eaque saepe voluptate odit soluta eveniet
+                          quam itaque officiis explicabo perferendis
+                        </p>
+                      </li>
+
+                      <li className="space-y-2 hover:bg-cgreen/10 rounded-xl p-2 transition-colors duration-300 ease-in-out">
+                        <h2 className="font-bold leading-none">Servicio 3</h2>
+                        <p className="line-clamp-2 text-sm leading-snug text-cgray">
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Ad eaque saepe voluptate odit soluta eveniet
+                          quam itaque officiis explicabo perferendis
+                        </p>
+                      </li>
+                    </ul>
+
+
+                  </div>
+                </div>
+              </div>
             </NavigationMenuContent>
           </NavigationMenuItem>
 
-		  <NavigationMenuItem>
+          <NavigationMenuItem>
             <NavigationMenuTrigger className="bg-transparent text-lg focus:bg-cgreen focus:text-white hover:bg-cpurple hover:text-white data-[state=open]:bg-cpurple/50 rounded-3xl">
               Adulto
             </NavigationMenuTrigger>
