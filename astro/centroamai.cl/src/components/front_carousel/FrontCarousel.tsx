8dea2b18f712cca89ec22b8f91b0b114435acbf0
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2024-06-03 13:43:20 -0400

    reordenamiento de footer de carousel
    Signed-off-by: figonzal1 <felipe.gonzalezalarcon94@gmail.com>

--- astro/centroamai.cl/src/components/front_carousel/FrontCarousel.tsx
+++ astro/centroamai.cl/src/components/front_carousel/FrontCarousel.tsx
@@ -11,12 +11,12 @@ const FrontCarousel = () => {
     <Carousel
       opts={{
         loop: true,
-        active: true,
+        active: false,
         duration: 60,
       }}
       plugins={[
         Autoplay({
-          delay: 5000,
+          delay: 10000,
           stopOnInteraction: false,
           stopOnMouseEnter: true,
         }),
@@ -24,27 +24,27 @@ const FrontCarousel = () => {
     >
       <CarouselContent>
         <CarouselItem>
-          <div className="relative">
-            <img
-              src={portada1.src}
-              alt="Home"
-              className="object-cover w-full h-[80vh] opacity-90 blur-sm rounded-b-3xl"
-            />
-            <div className="absolute inset-0 bg-gradient-to-br from-cbackground to-transparent opacity-100 rounded-b-3xl"></div>
+          <img
+            src={portada1.src}
+            alt="Home"
+            className="object-cover w-full h-[80vh] opacity-90 blur-sm rounded-b-3xl"
+          />
+          <div className="absolute inset-0 bg-gradient-to-br from-cbackground to-transparent opacity-100 rounded-b-3xl"></div>
 
-            <div className="absolute top-0 left-0 flex flex-col justify-center items-start p-20 w-full h-full">
-              <h2 className="text-2xl text-white">
-                Solicita tu hora con nuestros especialistas
-              </h2>
-              <h1 className="text-7xl text-white font-bold mb-5">
-                Centro amai
-              </h1>
-              <hr className="opacity-15 p-2 w-1/3 mb-3" />
+          <div className="absolute top-0 left-0 flex flex-col justify-center items-center lg:items-start p-10 sm:p-20  w-full h-full">
+            <h2 className="text-center md:text-left text-lg sm:text-2xl text-white">
+              Solicita tu hora con nuestros especialistas
+            </h2>
+            <h1 className="text-center text-5xl sm:text-7xl text-white font-bold mb-5">
+              Centro amai
+            </h1>
+            <hr className="opacity-15 p-2 w-5/6 lg:w-96 mb-3" />
 
-              <div className="relative w-1/4">
+            <div className="flex flex-col gap-5">
+              <div className="relative w-52 lg:w-96">
                 <div className="absolute w-full h-full bg-black rounded-full opacity-50 z-20 transform translate-x-1 translate-y-1.5"></div>
                 <a
-                  className="flex justify-center items-center bg-white hover:bg-cpurple rounded-full px-4 py-2 transition-colors duration-300 ease-in-out uppercase font-semibold z-20 relative text-cgreen hover:text-white"
+                  className="flex justify-center items-center bg-white hover:bg-cpurple rounded-full px-4 py-2 transition-colors duration-300 ease-in-out uppercase font-semibold z-20 relative text-cgreen hover:text-white text-center"
                   href="https://agendamiento.reservo.cl/makereserva/agenda/f0Jn8dB0F0pERa8N1U96tu94p2z5Uv"
                   target="_blank"
                   rel="noreferrer"
