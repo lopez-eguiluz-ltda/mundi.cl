328f5e3c5fba1f4999e4b63696f5dda00abe16ee
figonzal1 <felipe.gonzalez@rycconsultores.cl>
2025-05-06 17:46:14 -0400

    eliminación de personal y actualizacion de servicios

--- src/utils/teamsSeed.ts
+++ src/utils/teamsSeed.ts
@@ -2,24 +2,15 @@ import type { TeamType } from "./types";
 
 import camila from "@assets/images/equipo/camila_eguiluz.jpg";
 import karla from "@assets/images/equipo/karla_lopez.jpg";
-import mauro from "@assets/images/equipo/mauro_rivera.jpg";
-import carmina from "@assets/images/equipo/carmina_nicolai.jpg";
-import paulette from "@assets/images/equipo/paulette_toha.jpg";
 import claudia from "@assets/images/equipo/claudia_salas.jpg";
 import daniel from "@assets/images/equipo/daniel_oliva.jpg";
-import mariflor from "@assets/images/equipo/mariflor_martinez.jpg";
 import sebastian from "@assets/images/equipo/sebastian_vilchez.jpg";
-import gabriela from "@assets/images/equipo/gabriela_araya.jpg";
 import estrella from "@assets/images/equipo/estrella_daque.jpg";
 import felipe from "@assets/images/equipo/felipe_contreras.jpg";
 import jenifer from "@assets/images/equipo/jenifer_meneses.jpg";
-import charnelle from "@assets/images/equipo/charnelle_galleguillos.jpg";
-import valentina from "@assets/images/equipo/valentina_collao.jpg";
 import daniela_parra from "@assets/images/equipo/daniela_parra.jpg";
 import catalina_gamboa from "@assets/images/equipo/catalina_gamboa.jpg";
 import fernanda_maturana from "@assets/images/equipo/fernanda_maturana.jpg";
-import maria_vargas from "@assets/images/equipo/maria_vargas.jpg";
-import rocio_gimenez from "@assets/images/equipo/rocio_gimenez.jpg";
 
 //import { ImFacebook, ImLinkedin2 } from "react-icons/im";
 //import { SiInstagram } from "react-icons/si";
@@ -40,13 +31,6 @@ export const teams: TeamType[] = [
   },
 
   {
-    name: "Mauro Rivera",
-    position: "Medicina Biorreguladora",
-    photo: mauro,
-    socialLink: [],
-  },
-
-  {
     name: "Daniel Oliva",
     position: "Nutrición",
     photo: daniel,
@@ -54,37 +38,9 @@ export const teams: TeamType[] = [
   },
 
   {
-    name: "Carmina Nicolai",
-    position: "Matronería-Partería",
-    photo: carmina,
-    socialLink: [],
-  },
-
-  {
-    name: "Paulette Tohá",
-    position: "Psicología Adulto-Juvenil",
-    photo: paulette,
-    socialLink: [],
-  },
-
-  {
     name: "Claudia Salas",
     position: "Pediatría Ambulatoria",
     photo: claudia,
-    socialLink: [],
-  },
-
-  {
-    name: "Mariflor Martinez",
-    position: "Endocrinología Infantil",
-    photo: mariflor,
-    socialLink: [],
-  },
-
-  {
-    name: "Valentina Collao",
-    position: "Nutrición",
-    photo: valentina,
     socialLink: [],
   },
 
@@ -102,18 +58,6 @@ export const teams: TeamType[] = [
     socialLink: [],
   },
 
-  {
-    name: "Gabriela Araya",
-    position: "Psicopedagogía",
-    photo: gabriela,
-    socialLink: [],
-  },
-  {
-    name: "Rocío Giménez",
-    position: "Terapia Ocupacional",
-    photo: rocio_gimenez,
-    socialLink: [],
-  },
   {
     name: "Estrella Daque",
     position: "Terapia Ocupacional",
@@ -139,19 +83,6 @@ export const teams: TeamType[] = [
     name: "Jenifer Meneses",
     position: "Psicología Infantil",
     photo: jenifer,
-    socialLink: [],
-  },
-  {
-    name: "Mª José Vargas",
-    position: "Psicología Infanto-Juvenil",
-    photo: maria_vargas,
-    socialLink: [],
-  },
-
-  {
-    name: "Charnelle Galleguillos",
-    position: "Fonoaudiología / Consej. Lactancia",
-    photo: charnelle,
     socialLink: [],
   },
 
